c0e63eab04dfb0123eaa0d0f0d4c87761ca7a8b5
nogi kosuke <kskng0902@gmail.com>
2023-03-25 17:55:01 +0900

    createアクションを作成。動作確認、リダイレクトの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.create(post_params)
+    @post.save
+    redirect_to new_post_path
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
