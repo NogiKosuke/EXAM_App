426010fd846e746d5477fce3a6de68baacf75d86
nogi kosuke <kskng0902@gmail.com>
2023-03-25 20:42:37 +0900

    編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,26 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to posts_path
     else
-      render 'new'
+      render :new
     end
   end
 
   def show
-    @post =Post.find(params[:id])
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+
   end
 
   private
