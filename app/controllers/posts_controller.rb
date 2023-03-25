228575dc3897f6b6243573b48918770b428f1a9c
nogi kosuke <kskng0902@gmail.com>
2023-03-25 20:49:47 +0900

    削除機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,12 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
